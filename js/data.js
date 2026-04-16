6ed927577c36d70027a9e41fa8e0b9eb6c30eb2c
huandzzz <huanminh960@gmail.com>
2026-04-16 07:46:47 +0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,14 @@
+let products = [
+    {
+        name: "Camera Hikvision",
+        image: "images/camera1.jpg"
+    },
+    {
+        name: "Router WiFi",
+        image: "images/wifi.jpg"
+    },
+    {
+        name: "Bàn phím",
+        image: "images/keyboard.jpg"
+    }
+];
